6b2758ede64b9b9c60faebc72b3dd9e3b0339f4a
Debayan <debayanghosh408@gmail.com>
2023-11-27 01:49:44 +0530

    Drawing app using React

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Draw from './Draw';
+
+function App() {
+  return (
+    
+   
+    <div>
+      <Draw/>
+
+    </div>
+  
+  );
+}
+
+export default App;
---
+++ client/src/Draw.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Tldraw } from '@tldraw/tldraw'
+import '@tldraw/tldraw/tldraw.css'
+
+export default function Draw() {
+  return (
+    <div style={{ position: 'fixed', inset: 0 }}>
+			<Tldraw />
+		</div>
+  )
+}
